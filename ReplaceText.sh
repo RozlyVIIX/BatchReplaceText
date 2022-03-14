91c720f67792eab1980f1257d252561c7d382f52
Rozly <43327455+RozlyVIIX@users.noreply.github.com>
2022-03-14 16:53:06 +0800

    Add files via upload

---
+++ ReplaceText.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+old_str=""
+new_str=""
+filename="$1"
+bak_file="${filename}.bak"
+tmp_file="${filename}.tmp"
+
+if [[ -f $filename ]]; then
+  if [[ ! -f "$bak_file" ]]; then
+    echo "backup resource file to"
+    cp -r "$filename" "$bak_file"
+  fi
+  buffer=$(cat $filename | awk -v RS= '{gsub(/($old_str)/, "$new_str"); print}')
+  if [[ "$?" != 0 ]]; then
+    echo "failed to replace string, filename: $filename"
+  else
+    echo "$buffer" > "$tmp_file"
+    if [[ "$?" == 0 ]]; then
+      mv "$tmp_file" $filename || true
+    fi
+    echo "success"
+  fi
+else
+  echo "no such file or directory, $filename"
+fi
